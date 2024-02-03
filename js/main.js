5b39e423ea6d23591457640d7d8057eb50acb28d
Hillor Alal <hilloralal@h-MacBook-Air-sl-Hillor.local>
2024-02-03 13:03:14 +0200

    portfolioMatala

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // navigation bar function 
 function myMenuFanction(){
     var menuBtn = document.getElementById("MyNavMenu");
-
     if(menuBtn.className === "nav-menu"){
         menuBtn.className += "responsive";
     } else {
@@ -11,20 +10,16 @@ function myMenuFanction(){
 
 // shadow on navigation bar while scrolling 
 window.onscroll = function() {headerShadow()};
-
 function headerShadow(){
     const navHeader = document.getElementById("header");
-
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
         navHeader.style.height = "70px";
         navHeader.style.lineHeight = "70px";  
-
     } else {
         navHeader.style.boxShadow = "none";
         navHeader.style.height = "90px";
         navHeader.style.lineHeight = "90px";  
-
     }
 }
 
@@ -35,6 +30,7 @@ function headerShadow(){
     duration: 2000,
     reset: true     
 })
+
 //  home 
  sr.reveal('.featured-text-card',{})
  sr.reveal('.featured-name',{delay: 100})
